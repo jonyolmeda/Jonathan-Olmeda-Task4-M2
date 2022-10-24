5a1a7f2e93168e3ae027390bb41732f672598a0f
jonyolmeda <jonyolmeda@gmail.com>
2022-10-24 02:46:04 -0300

    Task 4 - Index.js funcionando con filtros cruzados

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,103 +3,61 @@ const $input = document.getElementById("input-texto");
 const $checkbox = document.getElementById(`checkbox`);
 
 let eventos;
-fetch('https://amazing-events.herokuapp.com/api/events')
-    .then( data => data.json())
-    .then( data => {
-      eventos = data.events;
-      crearCheckbox(eventos, $checkbox)
-    })
-.catch( err => console.log(err));
-
+fetch("https://amazing-events.herokuapp.com/api/events")
+  .then((data) => data.json())
+  .then((res) => {
+    eventos = res.events;
+    crearCheckbox(eventos, $checkbox);
+    imprimirTarjetas(eventos, $tarjetas);
+    $input.addEventListener(`keyup`,filtro)
+    $checkbox.addEventListener(`change`, filtro)
+  })
+  .catch((err) => console.log(err));
 
 function crearCheckbox(array, contenedor) {
-  let fn = e => e.category
-  let categorias = new Set(array.filter(fn).map(fn))
-  categorias.forEach(param => { $checkbox.innerHTML +=
-    `<div class="form-check form-check-inline" id="checkbox">
+  let fn = (e) => e.category;
+  let categorias = new Set(array.filter(fn).map(fn));
+  categorias.forEach((param) => {
+    contenedor.innerHTML += `<div class="form-check form-check-inline" id="checkbox">
     <input class="form-check-input" type="checkbox" id="${param}" value="${param}">
     <label class="form-check-label text-light" for="${param}">${param}</label>
-  </div>`
-  })
+  </div>`;
+  });
 }
 
- function crearTajeta(array) {
-
-  let div = document.createElement(`DIV`)
-  div.classList = "card col-3 bg-black"
-  div.innerHTML = `
+function crearTajeta(array) {
+  let div = document.createElement(`DIV`);
+  div.classList = "card col-3 bg-black";
+  div.innerHTML += `
       <div class="card-body">
           <p class="card-text text-light">${array.name}</p>
       </div>
       <img src="${array.image}" alt="Imagen de ${array.name}">
       <div class="card-body">
-          <p class="card-text text-light">${e.description}t.</p>
+          <p class="card-text text-light">${array.description}t.</p>
           <p class="card-text text-light">Price: $${array.price}</p>
       </div>
       <a href="details.html?id=${array._id}" class="btn btn-danger">Details</a>
   </div>
-`
-return div
- }
-
-function imprimrTarjetas(array, contenedor) {
-  let fragment = document.createDocumentFragment()
-  array.forEach(evento => fragment.appendChild(crearTajeta(evento)))
-  contenedor.appendChild(fragment)
+`;
+  return div;
 }
 
-
-
-
-/* function noCoincide(array, contenedor) {
-  if (array <= 0) {
-    contenedor.innerHTML = `
-    <h2 class="text-muted fs-4 fw-bold">Lo sentimos, no hay coincidencias</h2>
-    `;
+function imprimirTarjetas(array, contenedor) {
+  contenedor.innerHTML =``
+  if (array.length > 0) {
+    let fragment = document.createDocumentFragment();
+  array.forEach(e => fragment.appendChild(crearTajeta(e)));
+  contenedor.appendChild(fragment);
   }
+  else {
+    contenedor.innerHTML = `<h2  class="text-muted fs-4 fw-bold">Lo sentimos, no hay coincidencias</h2>`;
+  }  
 }
 
-input.addEventListener(`keyup`, (e) => {
-  let textoIngresado = e.target.value.toLowerCase()
-  elementosFiltrados = events.filter((nombres) =>
-    nombres.name.toLowerCase().includes(textoIngresado)
-  );
-  tarjetas.innerHTML = "";
-  noCoincide(elementosFiltrados, tarjetas);
-  imprimir(elementosFiltrados, tarjetas);
-});
-
-let categorias = new Set(events.map((objeto) => objeto.category));
-
-categorias.forEach((e) => {
-  checkbox.innerHTML += `
-    <div class="form-check form-check-inline" id="checkbox">
-      <input class="form-check-input" type="checkbox" id="${e}" value="${e}">
-      <label class="form-check-label text-light" for="inlineCheckbox1">${e}</label>
-    </div>
-  `;
-});
-
-let listaChequeada = [];
-
-checkbox.addEventListener(`change`, (e) => {
-  if (e.target.checked) {
-    listaChequeada = listaChequeada.concat(
-      events.filter((evento) =>
-        evento.category.toLowerCase().includes(e.target.id.toLowerCase())
-      )
-    );
-    tarjetas.innerHTML = "";
-    imprimir(listaChequeada, tarjetas);
-  } else if (!e.target.checked) {
-    listaChequeada = listaChequeada.filter(
-      (evento) => !evento.category.toLowerCase().includes(e.target.id.toLowerCase())
-    );
-    tarjetas.innerHTML = "";
-    imprimir(listaChequeada, tarjetas);
-  }
-  if (listaChequeada.length === 0) {
-    imprimir(events, tarjetas);
-  }
-});
- */
+function filtro (){
+  let checked = [...document.querySelectorAll('input[type="checkbox"]:checked')].map(e => e.value)
+  let eventosfiltrados = eventos.filter(e => checked.includes(e.category) || checked.length == 0)
+  let filtradosPorInput = eventosfiltrados.filter(valor => valor.name.toLowerCase().includes($input.value.toLowerCase()))
+  imprimirTarjetas(filtradosPorInput, $tarjetas)
+}
--- Scripts/stats.js
+++ Scripts/stats.js
@@ -1,1 +1,9 @@
 let contenedorTablas = document.getElementById(`contenedor-tablas`)
+
+fetch('https://amazing-events.herokuapp.com/api/events')
+    .then( data => data.json())
+    .then( data => {
+      let eventos = data.events;
+      
+    })
+.catch( err => console.log(err));
